0eb28f8fff2b785f7c979159a839d665c60949c0
Bình Minh <sunrise241196@gmail.com>
2024-01-25 03:02:25 +0700

    TestDeploy

--- src/main/java/com/swp391/project/ProjectApplication.java
+++ src/main/java/com/swp391/project/ProjectApplication.java
@@ -2,9 +2,17 @@ package com.swp391.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProjectApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
