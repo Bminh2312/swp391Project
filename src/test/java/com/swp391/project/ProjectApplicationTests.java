e79c5d67ee7ce05664cd1f71a2157a34849d1ab9
Bình Minh <sunrise241196@gmail.com>
2024-01-24 13:14:45 +0700

    secondcommit

---
+++ src/test/java/com/swp391/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
