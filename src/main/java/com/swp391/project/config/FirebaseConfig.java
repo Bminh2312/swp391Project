af93276c862ccebf20f85d3f1e122e60f74df97d
Bình Minh <sunrise241196@gmail.com>
2024-02-22 22:30:14 +0700

    DoneTashLoginGmail

--- src/main/java/com/swp391/project/config/FirebaseConfig.java
+++ src/main/java/com/swp391/project/config/FirebaseConfig.java
@@ -3,23 +3,38 @@ package com.swp391.project.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.swp391.project.ProjectApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
+import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Bean
+    @Value("${firebase.storage.bucket}")
+    private String storageBucket;
+
+    @Value("classpath:serviceAccountKey.json")
+    private Resource resource;
+
+    @PostConstruct
     public FirebaseApp firebaseApp () throws IOException {
-//        FileInputStream serviceAccount = new FileInputStream("classpath:swp391-f7197-firebase-adminsdk-qokx0-96c04d23a0.json");
-        InputStream serviceAccount = getClass().getResourceAsStream("/swp391-f7197-firebase-adminsdk-qokx0-96c04d23a0.json");
+        ClassLoader classLoader = ProjectApplication.class.getClassLoader();
+        FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+//        InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
+
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setStorageBucket("swp391-f7197.appspot.com")
+                .setCredentials(GoogleCredentials.fromStream(Objects.requireNonNull(serviceAccount)))
+                .setServiceAccountId("firebase-adminsdk-qokx0@swp391-f7197.iam.gserviceaccount.com")
+                .setStorageBucket(storageBucket)
                 .build();
 
         return FirebaseApp.initializeApp(options);
--- src/main/java/com/swp391/project/controller/LoginController.java
+++ src/main/java/com/swp391/project/controller/LoginController.java
@@ -1,18 +1,25 @@
 package com.swp391.project.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import com.swp391.project.service.LoginService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
 
     @Operation(summary = "This is to login the website")
     @ApiResponses(value = {
@@ -30,6 +37,11 @@ public class LoginController {
     public ResponseEntity<?> message(){
         String mess = "Hello World";
         return new ResponseEntity<>(mess, HttpStatus.OK);
+    }
+
+    @PostMapping("/signin/gmail")
+    public ResponseEntity<?> loginWithGmail(@RequestParam String accessToken) {
+        return loginService.loginWithGmail(accessToken);
     }
 
 }
