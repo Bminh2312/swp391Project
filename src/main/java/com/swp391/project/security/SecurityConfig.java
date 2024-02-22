e367097859f586f6ac508159ea99bb422c43a5f8
Bình Minh <105420232+Bminh2312@users.noreply.github.com>
2024-02-22 23:24:27 +0700

    Revert "DoneTashLoginGmail"

--- src/main/java/com/swp391/project/security/SecurityConfig.java
+++ src/main/java/com/swp391/project/security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer :: disable)
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(sessionManagement ->
                         sessionManagement
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
