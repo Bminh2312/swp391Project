fec88d01b59a95c7670e498c93385ec5d7e7f24f
Bình Minh <sunrise241196@gmail.com>
2024-03-06 22:54:34 +0700

    DoingTaskRoom

--- src/main/java/com/swp391/project/service/RoomService.java
+++ src/main/java/com/swp391/project/service/RoomService.java
@@ -1,2 +1,66 @@
-package com.swp391.project.service;public class RoomService {
+package com.swp391.project.service;
+
+import com.swp391.project.dto.RoomDTO;
+import com.swp391.project.entity.RoomEntity;
+import com.swp391.project.repository.RoomRepository;
+import com.swp391.project.service.impl.RoomServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+
+public class RoomService implements RoomServiceImp {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Override
+    public boolean create(String name) {
+        try{
+            TimeZone timeZone = TimeZone.getTimeZone("Asia/Ho_Chi_Minh");
+
+            // Lấy thời gian hiện tại dựa trên múi giờ của Việt Nam
+            Calendar calendar = Calendar.getInstance(timeZone);
+            Date currentTime = calendar.getTime();
+            RoomEntity roomEntity = new RoomEntity();
+            roomEntity.setName(name);
+            roomEntity.setCreatedAt(currentTime);
+            roomEntity.setUpdatedAt(currentTime);
+            roomEntity.setStatus("ACTIVE");
+            return true;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(int id, String name) {
+        try {
+            TimeZone timeZone = TimeZone.getTimeZone("Asia/Ho_Chi_Minh");
+            Optional<RoomEntity> roomEntity = roomRepository.findById(id);
+            if (roomEntity.isPresent()) {
+                // Lấy thời gian hiện tại dựa trên múi giờ của Việt Nam
+                Calendar calendar = Calendar.getInstance(timeZone);
+                Date currentTime = calendar.getTime();
+                roomEntity.get().setName(name);
+                roomEntity.get().setUpdatedAt(currentTime);
+                return true;
+            }else{
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public List<RoomDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String status) {
+        return false;
+    }
 }
--- src/main/java/com/swp391/project/service/impl/RoomServiceImp.java
+++ src/main/java/com/swp391/project/service/impl/RoomServiceImp.java
@@ -1,7 +1,17 @@
 package com.swp391.project.service.impl;
 
+import com.swp391.project.dto.RoomDTO;
+
+import java.util.List;
+
 public interface RoomServiceImp {
 
     boolean create (String name);
+
+    boolean update (int id, String name);
+
+    List<RoomDTO> findAll();
+
+    boolean delete (String status);
 
 }
