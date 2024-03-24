be11b17f527dfe83c3a0f90e1b2a93b7d9e62172
Bình Minh <sunrise241196@gmail.com>
2024-03-24 21:16:27 +0700

    FixRoomRepository

--- src/main/java/com/swp391/project/repository/RoomRepository.java
+++ src/main/java/com/swp391/project/repository/RoomRepository.java
@@ -11,8 +11,6 @@ import java.util.Optional;
 @Repository
 public interface RoomRepository extends JpaRepository<RoomEntity,Integer> {
 
-    List<RoomEntity> findByProjectRoom(ProjectEntity projectEntity);
-
     Optional<RoomEntity> findByName(String name);
 
 }
